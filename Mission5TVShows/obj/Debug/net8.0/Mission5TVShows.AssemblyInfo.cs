a722b37972c5f19a84796fa2cf23f376cbeb9564
McKenna Brown <153936426+mckennabrown0224@users.noreply.github.com>
2025-02-06 12:08:40 -0700

    test

--- Mission5TVShows/obj/Debug/net8.0/Mission5TVShows.AssemblyInfo.cs
+++ Mission5TVShows/obj/Debug/net8.0/Mission5TVShows.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission5TVShows")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ab7396db76e91af885aafd7f35501b120ba29c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission5TVShows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission5TVShows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
